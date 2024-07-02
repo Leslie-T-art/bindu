e1f195290e0f102ee6bb025d038ef39e9b0c1585
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-02 15:52:15 +0200

    Create Preloader.js

---
+++ bindu/components/elements/Preloader.js
@@ -0,0 +1,13 @@
+export default function Preloader() {
+    return (
+        <>
+            <div id="preloader">
+                <div className="preloader">
+                    <span />
+                    <span />
+                </div>
+            </div>
+
+        </>
+    )
+}
