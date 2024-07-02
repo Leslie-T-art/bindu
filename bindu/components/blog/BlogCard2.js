3bc59bdbf38db1def4bacfe5a67856b0b7582539
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-02 15:51:55 +0200

    Create BlogCard2.js

---
+++ bindu/components/blog/BlogCard2.js
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+export default function BlogCard2({ item }) {
+    return (
+        <>
+            <Link href={`/blog/${item.id}`}>
+                <img src={`/assets/images/blog/${item.img}`} alt="img" className="img-fluid" />
+            </Link>
+            <Link href={`/blog/${item.id}`} rel="bookmark">{item.title}</Link>
+            <br />
+
+        </>
+    )
+}
