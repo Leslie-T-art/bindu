7ad9d024f5a3145aaa1f1eda49b3b44bf3ee0df1
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-02 15:53:52 +0200

    Create Features.js

---
+++ bindu/components/sections/Features.js
@@ -0,0 +1,75 @@
+
+
+export default function Features() {
+    return (
+        <>
+            <section className="features-area pt-120 pb-15">
+                <div className="container">
+                    <div className="row fea-row">
+                        <div className="col-lg-2 col-md-4 col-6">
+                            <div className="tpfeatures tpfeaturesborder text-center mb-50">
+                                <div className="tpfeatures__icon mb-30">
+                                    <img src="assets/img/features/feature-icon-01.png" alt="features-icon" />
+                                </div>
+                                <div className="tpfeatures__content">
+                                    <h5 className="tpfeatures__title">Finest Skincare <br /> Lotions</h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-2 col-md-4 col-6">
+                            <div className="tpfeatures tpfeaturesborder text-center mb-50">
+                                <div className="tpfeatures__icon mb-30">
+                                    <img src="assets/img/features/feature-icon-02.png" alt="features-icon" />
+                                </div>
+                                <div className="tpfeatures__content">
+                                    <h5 className="tpfeatures__title">Finest Skincare <br /> Lotions</h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-2 col-md-4 col-6">
+                            <div className="tpfeatures tpfeaturesborder text-center mb-50">
+                                <div className="tpfeatures__icon mb-30">
+                                    <img src="assets/img/features/feature-icon-03.png" alt="features-icon" />
+                                </div>
+                                <div className="tpfeatures__content">
+                                    <h5 className="tpfeatures__title">Finest Skincare <br /> Lotions</h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-2 col-md-4 col-6">
+                            <div className="tpfeatures tpfeaturesborder text-center mb-50">
+                                <div className="tpfeatures__icon mb-30">
+                                    <img src="assets/img/features/feature-icon-04.png" alt="features-icon" />
+                                </div>
+                                <div className="tpfeatures__content">
+                                    <h5 className="tpfeatures__title">Finest Skincare <br /> Lotions</h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-2 col-md-4 col-6">
+                            <div className="tpfeatures tpfeaturesborder text-center mb-50">
+                                <div className="tpfeatures__icon mb-30">
+                                    <img src="assets/img/features/feature-icon-05.png" alt="features-icon" />
+                                </div>
+                                <div className="tpfeatures__content">
+                                    <h5 className="tpfeatures__title">Finest Skincare <br /> Lotions</h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-2 col-md-4 col-6">
+                            <div className="tpfeatures tpfeaturesborder text-center mb-50">
+                                <div className="tpfeatures__icon mb-30">
+                                    <img src="assets/img/features/feature-icon-06.png" alt="features-icon" />
+                                </div>
+                                <div className="tpfeatures__content">
+                                    <h5 className="tpfeatures__title">Finest Skincare <br /> Lotions</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </>
+    )
+}
