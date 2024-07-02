d8839c586d2951139659ed6fbd91e8308fe85ff5
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-02 15:54:08 +0200

    Create Services.js

---
+++ bindu/components/sections/Services.js
@@ -0,0 +1,58 @@
+
+
+export default function Services() {
+    return (
+        <>
+            <section className="services-area pt-70">
+                <div className="container">
+                    <div className="row services-gx-item">
+                        <div className="col-lg-3 col-sm-6">
+                            <div className="tpservicesitem d-flex align-items-center mb-30">
+                                <div className="tpservicesitem__icon mr-20">
+                                    <img src="/assets/img/svg/services01.svg" alt="" className="fn__svg" />
+                                </div>
+                                <div className="tpservicesitem__content">
+                                    <h4 className="tpservicesitem__title">Free shipping</h4>
+                                    <p>Free shipping on orders over.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-sm-6">
+                            <div className="tpservicesitem d-flex align-items-center mb-30">
+                                <div className="tpservicesitem__icon mr-20">
+                                    <img src="/assets/img/svg/services02.svg" alt="" className="fn__svg" />
+                                </div>
+                                <div className="tpservicesitem__content">
+                                    <h4 className="tpservicesitem__title">Free Returns</h4>
+                                    <p>30-days free return policy</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-sm-6">
+                            <div className="tpservicesitem d-flex align-items-center mb-30">
+                                <div className="tpservicesitem__icon mr-20">
+                                    <img src="/assets/img/svg/services03.svg" alt="" className="fn__svg" />
+                                </div>
+                                <div className="tpservicesitem__content">
+                                    <h4 className="tpservicesitem__title">Secured Payments</h4>
+                                    <p>We accept all major credit cards</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-sm-6">
+                            <div className="tpservicesitem d-flex align-items-center mb-30">
+                                <div className="tpservicesitem__icon mr-20">
+                                    <img src="/assets/img/svg/services04.svg" alt="" className="fn__svg" />
+                                </div>
+                                <div className="tpservicesitem__content">
+                                    <h4 className="tpservicesitem__title">Customer Service</h4>
+                                    <p>Top notch customer setvice</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
