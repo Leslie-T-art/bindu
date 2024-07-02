a8d38c3dacd11c14d7d6ea2bb7f91cb22ad0b85e
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-02 16:25:53 +0200

    Create page.js

---
+++ bindu/app/faq/page.js
@@ -0,0 +1,12 @@
+
+import Layout from "@/components/layout/Layout"
+export default function Faq() {
+
+    return (
+        <>
+            <Layout headerStyle={3} footerStyle={1} breadcrumbTitle="Faq">
+
+            </Layout>
+        </>
+    )
+}
