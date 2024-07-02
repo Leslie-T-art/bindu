7178b17edbf8b0549a24330308a99bbcd541130f
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-02 15:51:35 +0200

    Create page.js

---
+++ bindu/app/services/page.js
@@ -0,0 +1,12 @@
+
+import Layout from "@/components/layout/Layout"
+export default function Services() {
+
+    return (
+        <>
+            <Layout headerStyle={3} footerStyle={1} breadcrumbTitle="Services">
+
+            </Layout>
+        </>
+    )
+}
